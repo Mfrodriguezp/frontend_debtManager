068d81e9fca2777f87aee60ce6943c9301778fb1
Mfrodriguezp <mildriz06@gmail.com>
2022-03-02 14:04:53 -0500

    Bootstrap is added for project styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ManagerDebtsComponent } from './components/manager-debts/manager-debts.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'managerDebts',
+    component:ManagerDebtsComponent
+  },
+  {
+    path:'**',
+    component:ErrorPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ManagerDebtsComponent } from './components/manager-debts/manager-debts.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ManagerDebtsComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
