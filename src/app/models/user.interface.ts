584d991c456e7f90e973aa41aa47a56286d55c3c
Mfrodriguezp <mildriz06@gmail.com>
2022-06-29 22:58:03 -0500

    is has been added interfaces User and UserResponse for process response Backend

--- src/app/models/user.interface.ts
+++ src/app/models/user.interface.ts
@@ -1,15 +1,15 @@
-export type Roles = 'general' | 'admin';
+export type Roles = 'reader' | 'admin';
 
-export interface user{
-    userName: string;
-    password: string; 
+export interface User {
+    userName: string,
+    password: string
 }
 
-export interface userResponse {
-    message: string;
-    token: string;
-    idUsers: number;
-    role:  Roles;
+export interface UserResponse {
+    message: string,
+    token: string,
+    idUsers: number,
+    role:  Roles
 }
 
 export class User{};
