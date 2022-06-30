0c08cfcb699e987c64c850aa5cb0493d24438b8f
Mfrodriguezp <mildriz06@gmail.com>
2022-06-29 22:56:02 -0500

    is has been completed features login form success

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public title: String;
-  constructor() {
-    this.title="Titulo de prueba";
+  public userName: string;
+  public password: string;
+  constructor(
+    private authService: AuthService,
+    private _router: Router
+  ) {
+    this.title = "Titulo de prueba";
+    this.userName = "";
+    this.password = "";
+
   }
 
   ngOnInit(): void {
+
+  }
+  login() {
+    const dataUser = {
+      userName: this.userName,
+      password: this.password
+    };
+    this.authService.login(dataUser).subscribe(() => {
+      this._router.navigate(['/managerDebts']);
+    });
   }
 
 }
