c9e3b530c8d665fe9267e4bedd2ad8f6de081678
Mfrodriguezp <mildriz06@gmail.com>
2022-06-29 22:59:30 -0500

    is has been added FormsModule for two Databinding in forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { ManagerDebtsComponent } from './components/manager-debts/manager-debts.component';
 import { ErrorPageComponent } from './components/error-page/error-page.component';
 import { HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -19,7 +19,8 @@ import { HttpClientModule} from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
