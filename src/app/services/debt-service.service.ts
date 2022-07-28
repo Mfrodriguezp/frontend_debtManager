a4a006ff23b6107c8f9f6b7f753229f9b051a219
Mfrodriguezp <mildriz06@gmail.com>
2022-07-27 20:10:22 -0500

    has been refresh real time for total debts and getDebts after modified debt

--- src/app/services/debt-service.service.ts
+++ src/app/services/debt-service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
 import { global } from './global';
-import { Observable } from 'rxjs';
+import { Observable, Subject, tap } from 'rxjs';
 import { GetDebts } from '../models/debts.interface';
 
 @Injectable({
@@ -10,9 +10,15 @@ import { GetDebts } from '../models/debts.interface';
 export class DebtServiceService {
   public url = global.url;
 
+  private _refresh$ = new Subject<void>();
+
   constructor(
     private _http: HttpClient
   ) { }
+
+  get refresh$(){
+    return this._refresh$;
+  }
 
   getDebts(token:string):Observable<any>{
     let header = new HttpHeaders().set('auth', token);
@@ -26,7 +32,16 @@ export class DebtServiceService {
 
   editDebt(iddebt:any,debt:any,token:string):Observable<any>{
     let header = new HttpHeaders().set('auth',token);
-    return this._http.put(this.url+"editDebt/"+iddebt,debt,{headers:header});
+    return this._http.put(this.url+"editDebt/"+iddebt,debt,{headers:header,observe:'response'})
+    .pipe(
+      tap(()=>{
+        this._refresh$.next();
+      })
+    )
+  }
+
+  getTotalDebts():Observable<any>{
+    return this._http.get(this.url+"getTotalDebts");
   }
 
 }
